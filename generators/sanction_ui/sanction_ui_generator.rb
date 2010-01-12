ed34bb51e95fb15e29bd3bf3078b74049005c293
Joe Goggins <goggins@umn.edu>
2010-01-12 11:04:07 -0600

    generator working

--- generators/sanction_ui/sanction_ui_generator.rb
+++ generators/sanction_ui/sanction_ui_generator.rb
@@ -1,33 +1,21 @@
 class SanctionUiGenerator < Rails::Generator::Base
   def manifest
     record do |m|
-      SANCTION_UI_PLUGIN_BASE_PATH ||= "#{RAILS_ROOT}/vendor/plugins/sanction_ui"
-      SANCTION_UI_OVERRIDE_BASE_PATH ||= "#{RAILS_ROOT}/app/plugins/sanction_ui"
-      
-=begin
-      app/views/sanction_ui
-        assets
-        views
-
-      SANCTION_UI_PLUGIN_BASE_PATH ||=       
+      # Copy all sub-dirs into app that can be overridden when files
+      # with same rel paths are dropped in
+      #
       require 'find'
-      Find.find('vendor/plugins/sanction-ui/app/assets', 'vendor/plugins/sanction-ui/app/views') do |path|
+      Find.find("#{SanctionUi.plugin_base_path}/app/views", "#{SanctionUi.plugin_base_path}/app/assets") do |path|
         if FileTest.directory?(path)
           if File.basename(path)[0] == ?.
             Find.prune       # Don't look any further into this directory.
           else
-            puts "DIR #{path}"
+            final_target = SanctionUi.override_base_path + '/' + path.gsub(/^#{SanctionUi.plugin_base_path}\//,'')
+            m.directory final_target
           end
         end
       end
-        
-=end
-      m.directory "app/views/sanction_ui"
-      m.directory "public/sanction_ui_asset_overrides"
-      m.directory "public/sanction_ui_asset_overrides/images"
-      m.directory "public/sanction_ui_asset_overrides/js"
-      m.directory "public/sanction_ui_asset_overrides/css"
-      m.file 'public/sanction_ui_asset_overrides/README.txt', "public/sanction_ui_asset_overrides/README.txt"
+      m.file 'sanction_ui_overrides/README.txt', "#{SanctionUi.override_base_path}/README.txt"
     end
   end
 end
--- lib/sanction_ui.rb
+++ lib/sanction_ui.rb
@@ -1,2 +1,9 @@
+
 module SanctionUi
+  @@plugin_base_path = "vendor/plugins/sanction_ui"
+  mattr_accessor :plugin_base_path
+
+  @@override_base_path = "app/sanction_ui_overrides"
+  mattr_accessor :override_base_path
+  
 end
