8d3250c85d524eb8d45bd9e343dd8708d8aed44a
Joe Goggins <goggins@umn.edu>
2010-02-05 16:33:02 -0600

    Addedd access_denied_over route and handled accordingly in show view

--- app/controllers/sanction_ui/access_denied_controller.rb
+++ app/controllers/sanction_ui/access_denied_controller.rb
@@ -10,5 +10,19 @@ class SanctionUi::AccessDeniedController < SanctionUi::TopLevelController
       x.name == params[:role_def_or_perm_name].to_sym ||
       x.permissions.include?(params[:role_def_or_perm_name].to_sym)
     end
+    
+    unless params[:over_type].blank?
+      begin
+        @over_type = params[:over_type].constantize
+      rescue NameError => e
+        render :text => "Invalid over type" and return
+      end
+    end
+    
+    # Route is set up like :over_type/*over_id", hence the arrayness below
+    unless params[:over_id].blank?
+      @over_id = params[:over_id].first
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ ActionController::Routing::Routes.draw do |map|
     sanction_ui.describe "describe/:role_definition", :controller => 'main', :action => 'describe'
     sanction_ui.asset "asset/*relative_path", :controller => 'asset', :action => 'show'
     sanction_ui.access_denied "denied/:role_def_or_perm_name", :controller => 'access_denied', :action => 'show'
+    sanction_ui.access_denied_over "denied/:role_def_or_perm_name/:over_type/*over_id", :controller => 'access_denied', :action => 'show'
     sanction_ui.resources :roles,
       :only => [:index, :new, :create, :destroy]
   end
